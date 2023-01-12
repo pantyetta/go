7f5bda35dacfd7084a577368daefc032cf9aaac7
pantyetta <email@pantyetta.com>
2023-01-12 21:39:04 +0900

    別メソッドを作成する

--- tutorial/main.go
+++ tutorial/main.go
@@ -7,7 +7,13 @@ import (
 )
 
 func main() {
-	number1, _ := strconv.Atoi(os.Args[1])
-	number2, _ := strconv.Atoi(os.Args[2])
-	fmt.Println("Sum:", number1+number2)
+	sum := sum(os.Args[1], os.Args[2])
+	fmt.Println("Sum:", sum)
+}
+
+func sum(number1 string, number2 string) int {
+	int1, _ := strconv.Atoi(number1)
+	int2, _ := strconv.Atoi(number2)
+
+	return int1 + int2
 }
