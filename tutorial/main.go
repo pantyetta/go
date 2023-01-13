dc76b2e0709364614db8669c301624ae76ece0a6
pantyetta <email@pantyetta.com>
2023-01-13 14:07:05 +0900

    ポインタの使用

--- tutorial/main.go
+++ tutorial/main.go
@@ -1,19 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	sum := sum(os.Args[1], os.Args[2])
-	fmt.Println("Sum:", sum)
+	firstName := "John"
+	updateName(&firstName)
+	fmt.Println(firstName)
 }
 
-func sum(number1 string, number2 string) int {
-	int1, _ := strconv.Atoi(number1)
-	int2, _ := strconv.Atoi(number2)
-
-	return int1 + int2
+func updateName(name *string) {
+	*name = "David"
 }
