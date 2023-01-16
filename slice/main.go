26bf4a47e1431d9285feea4f6db5a06f626b146f
pantyetta <email@pantyetta.com>
2023-01-16 18:13:01 +0900

    slice append

--- slice/main.go
+++ slice/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	months := []string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
-	quarter2 := months[3:6]
-	quarter2Extended := quarter2[:4]
-	fmt.Println(quarter2, len(quarter2), cap(quarter2))
-	fmt.Println(quarter2Extended, len(quarter2Extended), cap(quarter2Extended))
+	var numbers []int
+	for i := 0; i < 10; i++ {
+		numbers = append(numbers, i)
+		fmt.Printf("%d\tcap=%d\t%v\n", i, cap(numbers), numbers)
+	}
 }
