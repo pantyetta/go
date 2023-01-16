d632dc8b800ee0172d94f7169f54a1f62487c62a
pantyetta <email@pantyetta.com>
2023-01-16 18:10:00 +0900

    slice 拡張 初めのポインタの位置に依存する

--- slice/main.go
+++ slice/main.go
@@ -4,12 +4,8 @@ import "fmt"
 
 func main() {
 	months := []string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
-	quarter1 := months[0:3]
 	quarter2 := months[3:6]
-	quarter3 := months[6:9]
-	quarter4 := months[9:12]
-	fmt.Println(quarter1, len(quarter1), cap(quarter1))
+	quarter2Extended := quarter2[:4]
 	fmt.Println(quarter2, len(quarter2), cap(quarter2))
-	fmt.Println(quarter3, len(quarter3), cap(quarter3))
-	fmt.Println(quarter4, len(quarter4), cap(quarter4))
+	fmt.Println(quarter2Extended, len(quarter2Extended), cap(quarter2Extended))
 }
