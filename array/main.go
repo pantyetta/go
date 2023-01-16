4a50c708caeb497c46df64e76b461ff5e4b9e94a
pantyetta <email@pantyetta.com>
2023-01-16 17:32:52 +0900

    array 配列の初期化

--- array/main.go
+++ array/main.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var a [3]int
-	a[1] = 10
-	fmt.Println(a[0])
-	fmt.Println(a[1])
-	fmt.Println(a[len(a)-1])
+	cities := [5]string{"New York", "Paris", "Berlin", "Madrid"}
+	fmt.Println("Cities:", cities)
 }
