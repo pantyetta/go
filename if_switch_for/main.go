121fe7f8843b2aa4956e00a5fca1646b5e04ed28
pantyetta <email@pantyetta.com>
2023-01-16 16:00:08 +0900

    複数if

--- if_switch_for/main.go
+++ if_switch_for/main.go
@@ -2,9 +2,16 @@ package main
 
 import "fmt"
 
+func givemeanumber() int {
+	return -1
+}
+
 func main() {
-	x := 27
-	if x%2 == 0 {
-		fmt.Println(x, "is even")
+	if num := givemeanumber(); num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has only one digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
 	}
 }
