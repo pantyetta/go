b9f7cf643d2cb266cc98002f5f5f036b7e44cc49
pantyetta <email@pantyetta.com>
2023-01-16 17:35:35 +0900

    array 最後の値のみの初期化

--- array/main.go
+++ array/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	cities := [...]string{"New York", "Paris", "Berlin", "Madrid"}
-	fmt.Println("Cities:", cities)
+	numbers := [...]int{99: -1}
+	fmt.Println("First Position:", numbers[0])
+	fmt.Println("Last Position:", numbers[99])
+	fmt.Println("Length:", len(numbers))
 }
