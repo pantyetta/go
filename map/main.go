34e0b2cc9884a02aef5002ab9be04f1d60de4c7f
pantyetta <email@pantyetta.com>
2023-01-16 18:31:20 +0900

    map

---
+++ map/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	studentsAge := map[string]int{
+		"john": 32,
+		"bob":  31,
+	}
+	// studentsAge := make(map[string]int)
+	fmt.Println(studentsAge)
+}
