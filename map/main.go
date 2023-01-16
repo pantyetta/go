f3a71f61267438b49e191e34ce6d78918a8a72be
pantyetta <email@pantyetta.com>
2023-01-16 18:44:44 +0900

    map delete

--- map/main.go
+++ map/main.go
@@ -4,12 +4,8 @@ import "fmt"
 
 func main() {
 	studentsAge := make(map[string]int)
-	studentsAge["John"] = 32
+	studentsAge["john"] = 32
 	studentsAge["bob"] = 31
-	age, exist := studentsAge["christy"]
-	if exist {
-		fmt.Println("Chisty's age is", age)
-	} else {
-		fmt.Println("Christy's age couldn't be found.")
-	}
+	delete(studentsAge, "john")
+	fmt.Println(studentsAge)
 }
