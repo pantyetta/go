05e31b9ff531b8a76341437d5a3a93f01f2af792
pantyetta <email@pantyetta.com>
2023-01-16 16:28:28 +0900

    for 実行条件だけ

--- if_switch_for/main.go
+++ if_switch_for/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	sum := 0
-	for i := 1; i <= 100; i++ {
-		sum += i
+	var num int64
+	rand.Seed(time.Now().Unix())
+	for num != 5 {
+		num = rand.Int63n(15)
+		fmt.Println(num)
 	}
-	fmt.Println("sum of 1..100 is", sum)
 }
