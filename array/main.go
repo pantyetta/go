147c369ded966c31e3d2386cfeb73d2d5bfb34a0
pantyetta <email@pantyetta.com>
2023-01-16 17:34:24 +0900

    array 省略記号による初期化

--- array/main.go
+++ array/main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	cities := [5]string{"New York", "Paris", "Berlin", "Madrid"}
+	cities := [...]string{"New York", "Paris", "Berlin", "Madrid"}
 	fmt.Println("Cities:", cities)
 }
