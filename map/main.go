80a4b4aa6ea741b7dd76fa4bad299730529e3053
pantyetta <email@pantyetta.com>
2023-01-16 18:44:06 +0900

    map 項目の有無

--- map/main.go
+++ map/main.go
@@ -6,5 +6,10 @@ func main() {
 	studentsAge := make(map[string]int)
 	studentsAge["John"] = 32
 	studentsAge["bob"] = 31
-	fmt.Println("Bob's age is", studentsAge["christy"])
+	age, exist := studentsAge["christy"]
+	if exist {
+		fmt.Println("Chisty's age is", age)
+	} else {
+		fmt.Println("Christy's age couldn't be found.")
+	}
 }
