5a9b551ee61f40f1ed55dc85510cd1c21baea3c0
pantyetta <email@pantyetta.com>
2023-01-16 16:21:06 +0900

    swtich 常に実行

--- if_switch_for/main.go
+++ if_switch_for/main.go
@@ -2,25 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-func location(city string) (string, string) {
-	var region string
-	var continent string
-	switch city {
-	case "Delhi", "Hyderabad", "Mumbai", "Chennai", "Kochi":
-		region, continent = "India", "Asia"
-	case "Lafayette", "Louisville", "Boulder":
-		region, continent = "Colorado", "USA"
-	case "Irvine", "Los Angeles", "San Diego":
-		region, continent = "California", "USA"
-	default:
-		region, continent = "Unknown", "Unknown"
-	}
-	return region, continent
-}
-
 func main() {
-	region, continent := location("Irvine")
-	fmt.Printf("John works in %s, %s\n", region, continent)
+	rand.Seed(time.Now().Unix())
+	r := rand.Float64()
+	switch {
+	case r > 0.1:
+		fmt.Println("Common case, 90% of the time")
+	default:
+		fmt.Println("10% of the time")
+	}
 }
