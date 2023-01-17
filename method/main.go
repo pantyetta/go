8b379d35e184eafb6f28e3dc15b9cbf4f35af91e
pantyetta <email@pantyetta.com>
2023-01-17 20:48:39 +0900

    method override オーバーラップしたstructを指定する．

--- method/main.go
+++ method/main.go
@@ -10,6 +10,11 @@ type square struct {
 	size int
 }
 
+type coloredTriangle struct {
+	triangle
+	color string
+}
+
 func (t triangle) perimeter() int {
 	return t.size * 3
 }
@@ -22,9 +27,12 @@ func (t *triangle) doubleSize() {
 	t.size *= 2
 }
 
+func (t coloredTriangle) perimeter() int {
+	return t.size * 3 * 2
+}
+
 func main() {
-	t := triangle{3}
-	t.doubleSize()
+	t := coloredTriangle{triangle{3}, "blue"}
 	fmt.Println("Size:", t.size)
-	fmt.Println("Perimeter:", t.perimeter())
+	fmt.Println("Perimeter", t.perimeter())
 }
