537e76173dbe2ed49fefd04ae40f7b031fea070e
pantyetta <email@pantyetta.com>
2023-01-18 17:53:01 +0900

    onlineStore Replace

--- training_onlinStore/main.go
+++ training_onlinStore/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Account struct {
 	fitstName, lastName string
+}
+
+type Employee struct {
+	Account
+	Credits float64
 }
 
 func (a *Account) ChangeName(firstName, lastName string) {
@@ -11,32 +19,50 @@ func (a *Account) ChangeName(firstName, lastName string) {
 	a.lastName = lastName
 }
 
-type Employee struct {
-	Account
-	credits float64
+func CreateEmployee(firstName, lastName string, credits float64) (*Employee, error) {
+	return &Employee{Account{firstName, lastName}, credits}, nil
 }
 
-func (e *Employee) AddCredits(credits float64) {
-	e.credits += credits
+func (e *Employee) AddCredits(credits float64) error {
+	if credits < 0.0 {
+		return errors.New("Invalid credits amount.")
+	}
+	e.Credits += credits
+	return nil
 }
 
-func (e *Employee) RemoveCredits(credits float64) {
-	e.credits -= credits
+func (e *Employee) RemoveCredits(credits float64) error {
+	if credits < 0.0 {
+		return errors.New("Invalid credits amount.")
+	}
+	if e.Credits < credits {
+		return errors.New("You can't remove credits than the account has.")
+	}
+	e.Credits -= credits
+	return nil
 }
 
 func (e Employee) CheckCredits() float64 {
-	return e.credits
+	return e.Credits
 }
 
 func (e Employee) String() string {
-	return fmt.Sprintf("%s%s", e.fitstName, e.lastName)
+	return fmt.Sprintf("Name: %s %s\nCredits: %.2f\n", e.fitstName, e.lastName, e.Credits)
 }
 
 func main() {
-	employee := Employee{Account{"pan", "cho"}, 30}
-	fmt.Println(employee)
-	employee.ChangeName("cho", "pan")
-	employee.AddCredits(100)
-	employee.RemoveCredits(50)
-	fmt.Println(employee.CheckCredits())
+	pan, _ := CreateEmployee("pan", "cho", 300)
+	fmt.Println(pan.CheckCredits())
+	err := pan.AddCredits(500)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	err = pan.RemoveCredits(2500)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	pan.ChangeName("pan", "tyetta")
+
+	fmt.Println(pan)
 }
