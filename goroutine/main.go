80d6d31617d977fe34bd13a390794a383dea8ddb
pantyetta <email@pantyetta.com>
2023-01-19 17:05:50 +0900

    gorutine select イベントの待機

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,42 +2,31 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"time"
 )
 
-func checkAPI(api string, ch chan string) {
-	_, err := http.Get(api)
-	if err != nil {
-		ch <- fmt.Sprintf("ERROR: %s is down!\n", api)
-		return
-	}
+func process(ch chan string) {
+	time.Sleep(3 * time.Second)
+	ch <- "Done processing!"
+}
 
-	ch <- fmt.Sprintf("SUCCESS: %s is up and running!\n", api)
+func replicate(ch chan string) {
+	time.Sleep(1 * time.Second)
+	ch <- "Done replicating!"
 }
 
 func main() {
-	start := time.Now()
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	go process(ch1)
+	go replicate(ch2)
 
-	apis := []string{
-		"https://management.azure.com",
-		"https://dev.azure.com",
-		"https://api.github.com",
-		"https://outlook.office.com/",
-		"https://api.somewhereintheinternet.com/",
-		"https://graph.microsoft.com",
+	for i := 0; i < 2; i++ {
+		select {
+		case process := <-ch1:
+			fmt.Println(process)
+		case replicate := <-ch2:
+			fmt.Println(replicate)
+		}
 	}
-
-	ch := make(chan string, 10)
-
-	for _, api := range apis {
-		go checkAPI(api, ch)
-	}
-
-	for i := 0; i < len(apis); i++ {
-		fmt.Print(<-ch)
-	}
-
-	elapsed := time.Since(start)
-	fmt.Printf("Done! It took %v seconds!\n", elapsed.Seconds())
 }
