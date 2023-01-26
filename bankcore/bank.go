cc0d57dbdb95023d04fa9bae608fe13457d08249
pantyetta <email@pantyetta.com>
2023-01-26 23:40:13 +0900

    bank 預ける ^& エラーテスト実装

--- bankcore/bank.go
+++ bankcore/bank.go
@@ -1,5 +1,7 @@
 package bank
 
+import "errors"
+
 // 顧客情報
 type Customer struct {
 	Name    string
@@ -12,4 +14,13 @@ type Account struct {
 	Customer
 	Number  int32
 	Balance float64
+}
+
+func (a *Account) Deposit(amount float64) error {
+	if amount < 0 {
+		return errors.New("the amount to deposit should be greater than zero")
+	}
+
+	a.Balance += amount
+	return nil
 }
