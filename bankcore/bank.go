7437d33d5ba3af2af39a55a22b3117c433956c43
pantyetta <email@pantyetta.com>
2023-01-26 23:29:29 +0900

    bank pass Account test

--- bankcore/bank.go
+++ bankcore/bank.go
@@ -1,5 +1,15 @@
 package bank
 
-func Hello() string {
-	return "Hey! I'm working!"
+// 顧客情報
+type Customer struct {
+	Name    string
+	Address string
+	Phone   string
+}
+
+// 顧客アカウント
+type Account struct {
+	Customer
+	Number  int32
+	Balance float64
 }
