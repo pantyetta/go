5ee99df60854b019a4bab32407853b5c4219c722
pantyetta <email@pantyetta.com>
2023-01-26 23:46:07 +0900

    bank 引き出し^& test method

--- bankcore/bank.go
+++ bankcore/bank.go
@@ -24,3 +24,16 @@ func (a *Account) Deposit(amount float64) error {
 	a.Balance += amount
 	return nil
 }
+
+func (a *Account) Withdraw(amount float64) error {
+	if amount <= 0 {
+		return errors.New("the amount to withdraw should be greater than zero")
+	}
+
+	if a.Balance < amount {
+		return errors.New("the amount to withdraw should be greater than the account's balance")
+	}
+
+	a.Balance -= amount
+	return nil
+}
